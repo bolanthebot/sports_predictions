b444ba688a394c78e63237728034554efc6889e8
Lukas-DS <lukasgcdsouza@gmail.com>
2025-12-23 07:28:41 -0500

    Update NBATeamFullPage.jsx
    includes game history on basic page
    - lots of formatting and css work to be done

--- sports_predictions/src/Pages/NBATeamFullPage.jsx
+++ sports_predictions/src/Pages/NBATeamFullPage.jsx
@@ -1,13 +1,44 @@
 import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import ObjectTable from "../Components/ObjectTable";
 
 export default function NBATeamFullPage() {
   const { state } = useLocation();
   const params = useParams();
   const team = state?.team;
+  const teamId = params.teamId;
+
+  const [teamGames, setTeamGames] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchTeamGames = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const res = await fetch(
+        "http://localhost:8000/api/nba/teams/?id=" + teamId
+      );
+      if (!res.ok) throw new Error("Failed to fetch team");
+
+      const data = await res.json();
+      const teamData = data || [];
+      setTeamGames(teamData);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTeamGames();
+  }, []);
+
+  console.log(teamGames);
 
   if (!team) return <div>Loading team {params.teamId}...</div>;
-  console.log(team);
 
   return (
     // ADD CSS
@@ -17,6 +48,8 @@ export default function NBATeamFullPage() {
       </h1>
       <h1 className="page-title">Key Dump:</h1>
       <ObjectTable data={team} />
+      <h1 className="page-title">Game History:</h1>
+      <ObjectTable data={teamGames} />
     </div>
   );
 }
