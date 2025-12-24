98d279fa5d6013124f55f9b0e0db1d745a45dc35
Lukas-DS <lukasgcdsouza@gmail.com>
2025-12-23 19:06:56 -0500

    WIP Game Page

--- sports_predictions/src/Pages/NBAGameFullPage.jsx
+++ sports_predictions/src/Pages/NBAGameFullPage.jsx
@@ -9,26 +9,57 @@ export default function NBAGameFullPage() {
   if (!game) return <div>Loading game {params.gameId}...</div>;
   console.log(game);
   return (
-    // ADD CSS
-    <div className="stuff">
-      <h1 className="page-title">Main Stuff:</h1>
-      <h1 className="more stuff">
-        {game.homeTeam.teamName} vs {game.awayTeam.teamName}
-      </h1>
-      <p>
-        Status:{" "}
-        {game.gameStatus === 1
-          ? "Scheduled"
-          : game.gameStatus === 2
-          ? "Live"
-          : "Final"}
-      </p>
-      <p>Status Details: {game.gameStatusText}</p>
-      <p>
-        Score: {game.awayTeam.score} — {game.homeTeam.score}
-      </p>
-      <h1 className="page-title">Key Dump:</h1>
-      <ObjectTable data={game} />
+    <div className="min-h-screen flex flex-col bg-slate-900 px-4">
+      <div className="max-w-md bg-slate-800 p-4">
+        <p className="mb-2 text-sm font-semibold text-gray-300">
+          {game.gameStatusText}
+        </p>
+        <div className="flex items-center justify-between">
+          <div className="text-left">
+            <p className="text-gray-200 text-xl font-bold">
+              {game.awayTeam.teamCity} {game.awayTeam.teamName}
+            </p>
+            <p className="text-white text-3xl font-bold">
+              {game.awayTeam.score}
+            </p>
+          </div>
+
+          <span className="text-sm font-medium text-gray-300">vs</span>
+
+          <div className="text-right">
+            <p className="text-gray-200 text-xl font-bold">
+              {game.homeTeam.teamCity} {game.homeTeam.teamName}
+            </p>
+            <p className="text-white text-3xl font-bold">
+              {game.homeTeam.score}
+            </p>
+          </div>
+        </div>
+        {game.gameClock && (
+          <p className="mt-2 text-center text-gray-300 font-medium">
+            {game.gameClock} - Q{game.period}
+          </p>
+        )}
+        {game.gameLeaders.awayLeaders.name && (
+          <div className="mt-3 space-y-1 text-sm">
+            <h1 className="text-gray-200 font-bold">Top Scorers:</h1>
+            <p className="text-gray-400">
+              <span className="font-semibold text-gray-200">
+                {game.gameLeaders.awayLeaders.teamTricode}
+              </span>{" "}
+              {game.gameLeaders.awayLeaders.name} —{" "}
+              {game.gameLeaders.awayLeaders.points} pts
+            </p>
+            <p className="text-gray-400">
+              <span className="font-semibold text-gray-200">
+                {game.gameLeaders.homeLeaders.teamTricode}
+              </span>{" "}
+              {game.gameLeaders.homeLeaders.name} —{" "}
+              {game.gameLeaders.homeLeaders.points} pts
+            </p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
