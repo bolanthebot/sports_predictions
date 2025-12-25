2038ebd691c14900a902726c9a1a9adf64afebd9
bolanthebot <80107770+bolanthebot@users.noreply.github.com>
2025-12-25 13:28:46 -0500

    Api fix

--- sports_predictions/backend/app.py
+++ sports_predictions/backend/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from services.nba import get_today_games,get_team,get_player
-
+import json
 app = FastAPI()
 
 # Allow frontend access
@@ -14,15 +14,15 @@ app.add_middleware(
 #gets todays games returns json
 @app.get("/api/nba/games/today")
 def today_games():
-    return get_today_games().to_json(orient='record')
+    return get_today_games()
 
 #gets team past games data by id eg http://localhost:8000/api/nba/teams/?id=1610612739
 #return json
 @app.get("/api/nba/teams/")
 def team_games(id):
-    return get_team(id).to_json(orient='record')
+    return get_team(id).to_json(orient='records')
 
 #gets player past games data by id eg http://localhost:8000/api/nba/players/?id=201935
 @app.get("/api/nba/players/")
 def player_games(id):
-    return get_player(id).to_json(orient='record')
+    return get_player(id).to_json(orient='records')
