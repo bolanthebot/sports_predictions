2d6475a47bc28576d7c88edc4d1471d20268e77f
Lukas-DS <lukasgcdsouza@gmail.com>
2026-01-09 02:04:01 -0500

    team page improvements

--- sports_predictions/src/Pages/NBATeamFullPage.jsx
+++ sports_predictions/src/Pages/NBATeamFullPage.jsx
@@ -2,6 +2,7 @@ import { useLocation, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Header from "../Components/Header.jsx";
 import ObjectTable from "../Components/ObjectTable";
+import KeyRow from "../Components/KeyRow.jsx";
 
 export default function NBATeamFullPage() {
   const { state } = useLocation();
@@ -37,20 +38,62 @@ export default function NBATeamFullPage() {
     fetchTeamGames();
   }, []);
 
-  console.log(teamGames);
+  console.log(typeof teamGames);
 
   if (!team) return <div>Loading team {params.teamId}...</div>;
 
   return (
     // ADD CSS
-    <div className="min-h-screen flex flex-col px-4 text-white">
-      <h1 className="page-title">
-        Team Page for {team.teamCity} {team.teamName}
-      </h1>
-      <h1 className="page-title">Key Dump:</h1>
-      <ObjectTable data={team} />
-      <h1 className="page-title">Game History:</h1>
-      <ObjectTable data={teamGames} />
+    <div className="p-4 flex flex-col">
+      <div className="min-w-2xl max-w-1/2 bg-slate-800 p-4 items-center">
+        <div className="flex text-3xl font-bold mb-4 text-white">
+          <h1>
+            {team.teamCity} {team.teamName}
+          </h1>
+          <h1 className="ml-2">- {team.teamTricode}</h1>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-12 gap-y-2 text-md mb-6">
+          <KeyRow label="teamId" value={team.teamId} />
+          <div></div>
+          <KeyRow label="Wins" value={team.wins} />
+          <KeyRow label="Losses" value={team.losses} />
+          <p className="text-white font-bold">Last/Current Game</p>
+          <div></div>
+          <KeyRow label="Score" value={team.score} />
+          <KeyRow label="Seed" value={team.seed ?? "null"} />
+          <KeyRow label="inBonus" value={team.inBonus} />
+          <KeyRow label="Timouts Remaining" value={team.timeoutsRemaining} />
+          <p className="text-white font-bold">Period Scores</p>
+          <div></div>
+          <KeyRow
+            label={"Period " + team.periods[0].period}
+            value={team.periods[0].score}
+          />
+          <KeyRow
+            label={"Period " + team.periods[1].period}
+            value={team.periods[1].score}
+          />
+          <KeyRow
+            label={"Period " + team.periods[2].period}
+            value={team.periods[2].score}
+          />
+          <KeyRow
+            label={"Period " + team.periods[3].period}
+            value={team.periods[3].score}
+          />
+        </div>
+      </div>
+      <div className="min-w-2xl max-w-1/2 bg-slate-800 p-4 items-center mt-6">
+        <div className="text-white font-bold mb-4">
+          <h1>Game History WIP</h1>
+        </div>
+        {/* <div className="grid grid-cols-1 gap-x-12 gap-y-2 text-md mb-6">
+          <KeyRow label="Total Games" value={teamGames.length} />
+          {teamGames.map((game) => (
+            <li>{game.Game_ID}</li>
+          ))}
+        </div> */}
+      </div>
     </div>
   );
 }
