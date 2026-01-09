0b90c388945729fa6ca44ece7b3331e59b4ef8e6
Lukas-DS <lukasgcdsouza@gmail.com>
2026-01-08 20:49:15 -0500

    Improvements to game page

--- sports_predictions/src/Pages/NBAGameFullPage.jsx
+++ sports_predictions/src/Pages/NBAGameFullPage.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
-import ObjectTable from "../Components/ObjectTable";
-import PredictionsMain from "../Components/PredictionsMain.jsx";
-import PredictionSlider from "../Components/PredictionSlider.jsx";
+import ActualGame from "../Components/GameFullPage/ActualGame.jsx";
 
 export default function NBAGameFullPage() {
   const { state } = useLocation();
@@ -41,71 +39,9 @@ export default function NBAGameFullPage() {
   if (error) return <div className="p-4 text-red-400">{error}</div>;
   if (!game) return <div>No game data</div>;
 
-  console.log(game);
   return (
-    <div className="bg-slate-900 min-h-screen flex flex-col px-4">
-      <div className="max-w-md bg-slate-800 p-4 items-center">
-        <p className="mb-2 text-sm font-semibold text-gray-300">
-          {game.gameStatusText}
-        </p>
-        <div className="flex items-center justify-between">
-          <div className="text-left">
-            <p className="text-gray-200 text-xl font-bold">
-              {game.awayTeam.teamCity} {game.awayTeam.teamName}
-            </p>
-            <p className="text-white text-3xl font-bold">
-              {game.awayTeam.score}
-            </p>
-            <PredictionsMain game={game.gameId} team={game.awayTeam.teamId} />
-          </div>
-
-          <span className="text-sm font-medium text-gray-300">vs</span>
-
-          <div className="text-right">
-            <p className="text-gray-200 text-xl font-bold">
-              {game.homeTeam.teamCity} {game.homeTeam.teamName}
-            </p>
-            <p className="text-white text-3xl font-bold">
-              {game.homeTeam.score}
-            </p>
-            <PredictionsMain game={game.gameId} team={game.homeTeam.teamId} />
-          </div>
-        </div>
-        {/* <div className="flex flex-col items-center mt-4">
-          <PredictionSlider
-            away={0.64}
-            home={0.35}
-            linelength={400}
-            lineheight={8}
-            dotSize={20}
-          />
-        </div>
- */}
-        {game.gameClock && (
-          <p className="mt-2 text-center text-gray-300 font-medium">
-            {game.gameClock} - Q{game.period}
-          </p>
-        )}
-        {game.gameLeaders.awayLeaders.name && (
-          <div className="mt-3 space-y-1 text-sm">
-            <h1 className="text-gray-200 font-bold">Top Scorers:</h1>
-            <p className="text-gray-400">
-              <span className="font-semibold text-gray-200">
-                {game.gameLeaders.awayLeaders.teamTricode}
-              </span>{" "}
-              {game.gameLeaders.awayLeaders.name} —{" "}
-              {game.gameLeaders.awayLeaders.points} pts
-            </p>
-            <p className="text-gray-400">
-              <span className="font-semibold text-gray-200">
-                {game.gameLeaders.homeLeaders.teamTricode}
-              </span>{" "}
-              {game.gameLeaders.homeLeaders.name} —{" "}
-              {game.gameLeaders.homeLeaders.points} pts
-            </p>
-          </div>
-        )}
-      </div>
+    <div className="bg-slate-900 flex flex-col px-4">
+      <ActualGame game={game} />
     </div>
   );
 }
